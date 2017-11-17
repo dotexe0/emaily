946c1ea95fabfeaedd2528a9b4482ba0a4e18064
dotexe0 <dotexe0@gmail.com>
2017-11-16 17:02:20 -0800

    minor changes to server file and dep

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
