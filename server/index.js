275add81c7648414a2095bfdc21eb9e4a7cc3905
Daniel Varela <dotexe0@gmail.com>
2017-11-17 17:09:09 -0800

    updated dev space and google auth strategy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import passport from 'passport';
+import GoogleStrategy from 'passport-google-oauth20'; // .Strategy
+
+import keys from './config/keys';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+},
+  (accessToken) => {
+    console.log(accessToken);
+  }
+));
+
+app.get('/auth/google', passport.authenticate('google', {
+  //only need user's profile and email, can request more (see docs)
+  scope: ['profile', 'email']
+}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
